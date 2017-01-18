ed26611a32fc3aaf9e5ad5d028a4de1c7f47fbe6
John Hardy <jh@lagado.com>
2017-01-18 15:46:14 +1100

    coverage6

---
+++ src/x.js
@@ -0,0 +1,4 @@
+const x = (a) => 123;
+
+export default x;
+ 
---
+++ src/x.test.js
@@ -0,0 +1,21 @@
+import x from './x'
+
+console.log('WHERE??????????',x);
+
+describe('Addition', () => {
+  // it('knows that 2 and 2 make 4', () => {
+  //   expect(2 + 2).toBe(4);
+  // });
+  // it('knows that 3 and 3 make 6', () => {
+  //   expect(3 + 3).toBe(6);
+  // });
+  it('is defined', () => {
+    expect(x).toBeDefined();
+  });
+  it('is a function', () => {
+    expect(typeof x).toBe('function');
+  });
+  it('to return 123', () => {
+    expect(x()).toBe(123);
+  });
+});
